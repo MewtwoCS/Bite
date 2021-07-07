055a4c50459b7441636ac88b3d4b0fb245ec6341
Kyung Lee <46550288+kyunglee1@users.noreply.github.com>
2021-07-07 10:28:16 -0700

    Format App component

--- client/components/App/index.jsx
+++ client/components/App/index.jsx
@@ -3,9 +3,9 @@ import { io } from 'socket.io-client';
 import './styles.css';
 
 const App = () => {
-  const socket = io('http://localhost:5001');
+	const socket = io('http://localhost:5001');
 
-  return <div id="test-id">Hello from the App component</div>;
+	return <div id='test-id'>Hello from the App component</div>;
 };
 
 export default App;
