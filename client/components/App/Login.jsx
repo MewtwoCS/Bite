21611daff885b5cabec957c58c1ab2f30c63d745
Eric Park <ericparkmusic@gmail.com>
2021-07-07 10:16:54 -0700

    Created login component

---
+++ client/components/App/Login.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  return <div id="test-id">Hello from the Login component</div>;
+};
+
+export default Login;
--- client/components/App/index.jsx
+++ client/components/App/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { io } from 'socket.io-client';
+import Login from './Login.jsx';
 import './styles.css';
 
 const App = () => {
   const socket = io('http://localhost:5001');
 
-  return <div id="test-id">Hello from the App component</div>;
+  return (
+    <>
+      <div id="test-id">Hello from the App component</div>
+      <Login></Login>
+    </>
+  );
 };
 
 export default App;
