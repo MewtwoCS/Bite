448a9e231e3e0872a0b77779c6ef0b90b7c934cf
Eric Park <ericparkmusic@gmail.com>
2021-07-07 01:17:52 -0700

    Set up HMR while in dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,52 @@
 const path = require('path');
 
 module.exports = (env) => {
-	const isProduction = env.production ? true : false;
-	const mode = isProduction ? 'production' : 'development';
+  const isProduction = env.production ? true : false;
+  const mode = isProduction ? 'production' : 'development';
+  return {
+    entry: './client/index.js',
 
-	return {
-		entry: './client/index.js',
+    target: 'web',
 
-		target: 'web',
+    mode,
 
-		mode,
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: '/',
+    },
 
-		output: {
-			path: path.resolve(__dirname, 'dist'),
-			filename: 'bundle.js',
-			publicPath: '/',
-		},
+    // devServer: {
+    //   contentBase: path.join(__dirname, 'dist'),
+    //   compress: true,
+    //   hot: true,
+    //   port: 8080,
+    // },
 
-		devServer: {
-			contentBase: path.join(__dirname, 'dist'),
-			compress: true,
-			hot: true,
-			port: 8080,
-		},
+    module: {
+      rules: [
+        {
+          test: /\.jsx?/,
+          exclude: /nodeModules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
 
-		module: {
-			rules: [
-				{
-					test: /\.jsx?/,
-					exclude: /nodeModules/,
-					use: {
-						loader: 'babel-loader',
-						options: {
-							presets: ['@babel/preset-env', '@babel/preset-react'],
-						},
-					},
-				},
-				{
-					test: /\.css$/,
-					use: ['style-loader', 'css-loader'],
-				},
-			],
-		},
-	};
+    devServer: {
+      publicPath: '/dist/',
+      proxy: {
+        '/api': 'http://localhost:5000',
+      },
+    },
+  };
 };
