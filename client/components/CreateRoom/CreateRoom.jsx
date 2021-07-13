04fece628433bff3b35dde7992ceab17d6054c7d
Kyung Lee <46550288+kyunglee1@users.noreply.github.com>
2021-07-12 20:28:13 -0700

    Add styling

--- client/components/CreateRoom/CreateRoom.jsx
+++ client/components/CreateRoom/CreateRoom.jsx
@@ -1,107 +1,129 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { AppContext } from '../../AppContext';
+import { SocketContext } from '../../SocketContext';
+import { Button, TextField, Checkbox } from '@material-ui/core';
+import './styles.css';
 
 const CreateRoom = () => {
-  const history = useHistory();
+	const history = useHistory();
+	const socket = useContext(SocketContext);
 
-  const {
-    setLocation, setRoomName, setRoomId, location, roomName, roomId,
-  } = useContext(AppContext);
-  // const [location, setLocation] = useState('');
-  // const [roomName, setRoomName] = useState('');
-  const [preferences, setPreferences] = useState({
-    fastFood: false,
-    korean: false,
-    chinese: false,
-    medi: false,
-    mexican: false,
-  });
+	const {
+		setLocation,
+		setRoomName,
+		setRoomId,
+		location,
+		roomName,
+		roomId,
+		usernameVal,
+		isHost,
+		setIsHost,
+	} = useContext(AppContext);
 
-  useEffect(() => {
-    if (roomId) {
-      history.push('/wait'.concat(`${roomId}`));
-    }
-  })
+	const [preferences, setPreferences] = useState({
+		fastFood: false,
+		korean: false,
+		chinese: false,
+		medi: false,
+		mexican: false,
+	});
 
-  const handleLocationChange = (e) => {
-    setLocation(e.target.value);
-  };
+	useEffect(() => {
+		if (roomId && isHost) {
+			socket.emit(
+				'create',
+				socket.id,
+				usernameVal,
+				'room'.concat(` ${roomId}`),
+				location
+			);
+			history.push('/wait'.concat(`${roomId}`));
+		}
+	});
 
-  const handleRoomChange = (e) => {
-    setRoomName(e.target.value);
-  };
+	const handleLocationChange = (e) => {
+		setLocation(e.target.value);
+	};
 
-  const handlePrefChange = (e) => {
-    const { name } = e.target;
-    const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+	const handleRoomChange = (e) => {
+		setRoomName(e.target.value);
+	};
 
-    setPreferences((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
+	const handlePrefChange = (e) => {
+		const { name } = e.target;
+		const value =
+			e.target.type === 'checkbox' ? e.target.checked : e.target.value;
 
-  const handleClick = () => {
-    const roomVar = Math.random() * 100;
+		setPreferences((prev) => ({
+			...prev,
+			[name]: value,
+		}));
+	};
 
-    setRoomId(Math.ceil(roomVar));
-  };
+	const handleClick = () => {
+		if (location.length > 0) {
+			const roomVar = Math.random() * 100;
+			setRoomId(Math.ceil(roomVar));
+			setIsHost(true);
+		}
+	};
 
-  return (
-    <div>
-      Location:
-      {' '}
-      <input
-        id="location-input"
-        type="text"
-        value={location}
-        onChange={handleLocationChange}
-      />
-      <fieldset>
-        <legend>Preferences</legend>
-        <input
-          name="fastFood"
-          type="checkbox"
-          checked={preferences.fastFood}
-          onChange={handlePrefChange}
-        />
-        Fast Food
-        <input
-          name="korean"
-          type="checkbox"
-          checked={preferences.korean}
-          onChange={handlePrefChange}
-        />
-        Korean
-        <input
-          name="chinese"
-          type="checkbox"
-          checked={preferences.chinese}
-          onChange={handlePrefChange}
-        />
-        Chinese
-        <input
-          name="medi"
-          type="checkbox"
-          checked={preferences.medi}
-          onChange={handlePrefChange}
-        />
-        Mediterannean
-        <input
-          name="mexican"
-          type="checkbox"
-          checked={preferences.mexican}
-          onChange={handlePrefChange}
-        />
-        Mexican
-      </fieldset>
-      Room Name:
-      {' '}
-      <input type="text" value={roomName} onChange={handleRoomChange} />
-      <button onClick={handleClick}>Create Room</button>
-    </div>
-  );
+	return (
+		<div id='create-container'>
+			<div id='location-input' className='create'>
+				LOCATION:
+				<TextField
+					type='text'
+					value={location}
+					onChange={handleLocationChange}
+				/>
+			</div>
+			<fieldset id='preferences' className='create'>
+				<legend>
+					<b>Preferences</b>
+				</legend>
+				<Checkbox
+					name='fastFood'
+					type='checkbox'
+					checked={preferences.fastFood}
+					onChange={handlePrefChange}
+				/>
+				Fast Food
+				{/* <Checkbox
+					name='korean'
+					type='checkbox'
+					checked={preferences.korean}
+					onChange={handlePrefChange}
+				/> */}
+				{/* Korean */}
+				<Checkbox
+					name='chinese'
+					type='checkbox'
+					checked={preferences.chinese}
+					onChange={handlePrefChange}
+				/>
+				Chinese
+				<Checkbox
+					name='medi'
+					type='checkbox'
+					checked={preferences.medi}
+					onChange={handlePrefChange}
+				/>
+				Mediterannean
+				<Checkbox
+					name='mexican'
+					type='checkbox'
+					checked={preferences.mexican}
+					onChange={handlePrefChange}
+				/>
+				Mexican
+			</fieldset>
+			<Button color='primary' onClick={handleClick} className='create'>
+				Create Room
+			</Button>
+		</div>
+	);
 };
 
 export default CreateRoom;
--- server/socket/socket.js
+++ server/socket/socket.js
@@ -3,66 +3,86 @@ const sock = {};
 
 const users = [];
 const votes = [];
+let newLocation = '';
 
 const getUsersInRoom = (room) => users.filter((user) => user.room === room);
 
 sock.init = (PORT) => {
-  const io = require('socket.io')(PORT, {
-    cors: {
-      origin: ['http://localhost:8080'],
-    },
-  });
+	const io = require('socket.io')(PORT, {
+		cors: {
+			origin: ['http://localhost:8080'],
+		},
+	});
 
-  io.on('connection', (socket) => {
-    console.log(`SOCKET.IO --> ${socket.id}: connected`);
+	io.on('connection', (socket) => {
+		console.log(`SOCKET.IO --> ${socket.id}: connected`);
 
-    socket.on('create', (clientId, username, room) => {
-      users.push({
-        clientId,
-        username,
-        room,
-        hasVoted: false,
-      });
-      socket.join(room);
-      io.to(room).emit('roomData', users);
-    });
+		socket.on('location', (newLocation) => {
+			location = newLocation;
+			io.emit('location', newLocation);
+		});
 
-    socket.on('start-game', (room) => {
-      io.to(room).emit('begin-game');
-    });
+		socket.on('create', (clientId, username, room, location) => {
+			users.push({
+				clientId,
+				username,
+				room,
+				hasVoted: false,
+			});
+			socket.join(room);
+			newLocation = location;
+			io.to(room).emit('roomData', users, newLocation);
+		});
 
-    socket.on('submit-vote', (room, id, voteArr) => {
-      if (votes.length === 0) votes.push(...voteArr);
-      else {
-        for (let i = 0; i < voteArr.length; i++) {
-          votes[i] += voteArr[i];
-        }
-      }
+		socket.on('join', (clientId, username, room) => {
+			users.push({
+				clientId,
+				username,
+				room,
+				hasVoted: false,
+			});
+			socket.join(room);
+			io.to(room).emit('roomData', users, newLocation);
+		});
 
-      const voter = users.find((user) => {
-        if (user.clientId === id) {
-          user.hasVoted = true;
-          return user;
-        }
-        return null;
-      });
+		socket.on('start-game', (room) => {
+			io.to(room).emit('begin-game');
+		});
 
-      const participants = getUsersInRoom(room);
-      const voters = participants.filter((user) => user.hasVoted);
-      if (voters.length === participants.length) {
-        io.to(room).emit('all-votes-in', votes);
-      } else {
-        io.to(room).emit('submitted', voter);
-      }
-    });
+		socket.on('submit-vote', (room, id, voteArr) => {
+			if (votes.length === 0) votes.push(...voteArr);
+			else {
+				for (let i = 0; i < voteArr.length; i++) {
+					votes[i] += voteArr[i];
+				}
+			}
 
-    // socket.on('disconnect', () => {
-    //   io.to(user.room).emit('roomData', {
-    //     room: user.room,
-    //     users: getUsersInRoom(user.room),
-    //   });
-    // });
-  });
+			const voter = users.find((user) => {
+				if (user.clientId === id) {
+					user.hasVoted = true;
+					return user;
+				}
+				return null;
+			});
+
+			const participants = getUsersInRoom(room);
+			const voters = participants.filter((user) => user.hasVoted);
+			if (voters.length === participants.length) {
+				io.to(room).emit('all-votes-in', votes);
+				users.length = 0;
+				votes.length = 0;
+			} else {
+				io.to(room).emit('submitted', voter);
+			}
+		});
+
+		// socket.on('disconnect', () => {
+		//   io.to(user.room).emit('roomData', {
+		//     room: user.room,
+		//     users: getUsersInRoom(user.room),
+		//   });
+		// });
+	});
 };
 
 module.exports = sock;
